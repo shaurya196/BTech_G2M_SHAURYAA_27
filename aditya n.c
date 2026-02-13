a95ea40509d6fcdc6d6b2675a6975395d715f107
shaurya196 <ss1990.shaurya@gmail.com>
2026-02-13 15:22:45 +0530

    Add files via upload
    This program greatest between two numbers

---
+++ aditya n.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+int main()
+    {
+    int mark1,mark2,mark3,mark4,mark5,sum,average,percentage;
+     printf("enter mark 1");
+     scanf("%d",mark1);
+     printf("enter mark 2");
+     scanf("%d",mark2);
+     printf("enter mark 3");
+     scanf("%d",mark3);
+     printf("enter mark 4");
+     scanf("%d",mark4);
+     printf("enter mark 5");
+     scanf("%d",mark5);
+     sum=mark1+mark2+mark3+mark4+mark4
+
+   return0;
+ }
